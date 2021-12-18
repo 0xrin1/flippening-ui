7f7809b3b7fa869fb50844e8e011b7b63b0d3112
Martin Stuessy <contact@martinstuessy.com>
2021-12-18 14:15:36 +0000

    replace bootstrap badge pill with mui chip component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import Head from 'next/head';
-import Badge from 'react-bootstrap/Badge'
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import { AccountsContext } from '../context/AccountContext';
 import OnClickConnect from '../components/OnClickConnect';
+import Chip from '@mui/material/Chip';
 
 type Props = {};
 
@@ -18,25 +18,25 @@ const Header = ({}: Props) => {
             </Head>
             <AccountsContext.Consumer>
                 {(context: any) => (
-                    <Navbar variant="dark" bg="dark" expand="lg" className="mb-4">
+                    <Navbar expand="lg" className="mb-4">
                         <Container>
                             <Navbar.Brand>Flippening</Navbar.Brand>
                             <Nav className="justify-content-end" style={{ width: "100%" }}>
                                 <OnClickConnect />
-                                <Badge pill bg="secondary">
-                                    {context.accounts && context.accounts.map((account: any) => {
+                                <Chip label={
+                                    context.accounts && context.accounts.map((account: any) => {
                                         return <div key={account.balance}>
                                             <div>{account.balance}</div>
                                         </div>;
-                                    })}
-                                </Badge>
-                                <Badge pill bg="secondary">
-                                    {context.accounts && context.accounts.map((account: any) => {
+                                    })
+                                } />
+                                <Chip label={
+                                    context.accounts && context.accounts.map((account: any) => {
                                         return <div key={account.address}>
                                             <div>{account.address}</div>
                                         </div>;
-                                    })}
-                                </Badge>
+                                    })
+                                } />
                             </Nav>
                         </Container>
                     </Navbar>
